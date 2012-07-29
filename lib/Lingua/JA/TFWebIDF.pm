2099a635ab57ef07002ce3ad753e260b46227fe1
Kohei Yoshioka 🈔🈬🈭🈮 <pawapawa@cpan.org>
2012-07-29 10:09:09 +0000

    Import of PAWAPAWA/Lingua-JA-TFWebIDF-0.35 from CPAN.
    gitpan-cpan-distribution: Lingua-JA-TFWebIDF
    gitpan-cpan-version:      0.35
    gitpan-cpan-path:         PAWAPAWA/Lingua-JA-TFWebIDF-0.35.tar.gz
    gitpan-cpan-author:       PAWAPAWA
    gitpan-cpan-maturity:     released

--- lib/Lingua/JA/TFWebIDF.pm
+++ lib/Lingua/JA/TFWebIDF.pm
@@ -12,7 +12,7 @@ use Text::MeCab;
 use Lingua::JA::Halfwidth::Katakana;
 use Lingua::JA::TFWebIDF::Result;
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 
 sub new
@@ -550,7 +550,7 @@ a hash reference which contains terms and their TF scores.
 
 Calculates TF score via MeCab.
 
-=head2 idf, df, purge
+=head2 idf, df, purge, db_open, db_close
 
 See L<Lingua::JA::WebIDF>.
 
@@ -563,6 +563,8 @@ pawa E<lt>pawapawa@cpan.orgE<gt>
 L<Lingua::JA::WebIDF>
 
 L<Lingua::JA::WebIDF::Driver::TokyoTyrant>
+
+L<Lingua::JA::TermExtractor>
 
 =head1 LICENSE
 
